2562f5d15140c941e313c429e5bc14051c5d547e
Thomas Northall-Little <tomnlittle@outlook.com>
2017-12-26 11:21:06 +1100

    Refactored Logger

--- src/logging/index.js
+++ src/logging/index.js
@@ -21,69 +21,6 @@ const customLevels = {
   }
 };
 
-// Single global variable
-let winstonLogger;
-
-/**
- * Instantiates the winston logger and initialises the logs folder 
- */
-const instantiate = function() {
-  const logsFolder = conf.LOGS_FOLDER;
-  const debugFolder = logsFolder + 'debug/';
-  const alertsFolder = logsFolder + 'alerts/';
-
-  // Create the logs folder
-  try {
-    fs.mkdirSync(logsFolder);
-    fs.mkdirSync(debugFolder);
-    fs.mkdirSync(alertsFolder);
-  } catch (err) {
-    // If the err code is not the fact the folder already exists
-    if (err.code !== 'EEXIST') {
-      console.error('[app] Could not create folders" ', {
-        err: err
-      });
-      process.exit(1);
-    }
-  }
-
-  const filename = moment().toISOString();
-  const debug = debugFolder + filename;
-  const alert = alertsFolder + filename;
-
-  return createLogger({
-    levels: customLevels.levels,
-    transports: [
-      new transports.Console({ 
-        levels: customLevels.levels,
-        level: 'error',
-        format: format.combine(
-          colorize(),
-          consoleFormatter
-        ),
-        colorize : true
-      }),
-      new transports.File({ 
-        format: format.combine(
-          isPrivate(),
-          consoleFormatter
-        ),
-        filename: debug,
-        colorize : false
-      }),
-      new transports.File({ 
-        level: 'alert',
-        format: format.combine(
-          isPrivate(),
-          consoleFormatter
-        ),
-        filename: alert,
-        colorize : false
-      })
-    ]
-  });
-};
-
 /**
  * Handler for private error logs
  * @param  {} info - object
@@ -95,100 +32,159 @@ const isPrivate = format((info) => {
 });
 
 /**
- * Handler for formatting the console output
- * @param  {} info - object
- */
+* Handler for formatting the console output
+* @param  {} info - object
+*/
 const consoleFormatter = printf((info) => {
 
   let errString = '';
   Object.keys(info.err).forEach((key) => {
     if (info.err[key]) {
       errString += info.err[key];
-    } 
+    }
   });
 
   const timestamp = moment().format(conf.DISPLAY_DATE_FORMAT);
-  
+
   return `${timestamp} ${info.level}: ${info.message} ${errString}`;
 });
 
-/**
- * Returns the error object, so we always print the full stack of the error
- * @param  {} err - Error Object
- */
-const prepareError = function(error) {
-  if (!error) return {};
+
+class Logger {
+  /**
+   * Instantiates the winston logger and initialises the logs folder 
+   */
+  constructor() {
+    const logsFolder = conf.LOGS_FOLDER;
+    const debugFolder = logsFolder + 'debug/';
+    const alertsFolder = logsFolder + 'alerts/';
+
+    // Create the logs folder
+    try {
+      fs.mkdirSync(logsFolder);
+      fs.mkdirSync(debugFolder);
+      fs.mkdirSync(alertsFolder);
+    } catch (err) {
+      // If the err code is not the fact the folder already exists
+      if (err.code !== 'EEXIST') {
+        console.error('[app] Could not create folders" ', {
+          err: err
+        });
+        process.exit(1);
+      }
+    }
+
+    const filename = moment().toISOString();
+    const debug = debugFolder + filename;
+    const alert = alertsFolder + filename;
+
+    this.logger = createLogger({
+      levels: customLevels.levels,
+      transports: [
+        new transports.Console({ 
+          levels: customLevels.levels,
+          level: 'error',
+          format: format.combine(
+            colorize(),
+            consoleFormatter
+          ),
+          colorize : true
+        }),
+        new transports.File({ 
+          format: format.combine(
+            isPrivate(),
+            consoleFormatter
+          ),
+          filename: debug,
+          colorize : false
+        }),
+        new transports.File({ 
+          level: 'alert',
+          format: format.combine(
+            isPrivate(),
+            consoleFormatter
+          ),
+          filename: alert,
+          colorize : false
+        })
+      ]
+    });
+  }
+
+
+  /**
+   * This is the main function called
+   * @param  {} msg - Any string that needs be printed
+   * @param  {} object  - Error object
+   * @param  {} privateMode - Whether the log should be written to disk or not 
+   */
+  log(msg, object, level, privateMode = false) {
   
-  const err = JSON.stringify(error) || null;
-  const message = error.message || null;
-  const stack = error.stack || null;
-  
-  return {
-    err, 
-    message, 
-    stack,
-  };
-};
+    const error = this.prepareError(object);
+    
+    return this.logger.log({
+      private: privateMode,
+      level: level,
+      message: msg, 
+      err: error
+    });
+  }
 
-/**
- * This is the main function called
- * @param  {} msg - Any string that needs be printed
- * @param  {} object  - Error object
- * @param  {} privateMode - Whether the log should be written to disk or not 
- */
-const log = function(msg, object, level, privateMode = false) {
-  if (!winstonLogger) winstonLogger = instantiate();
-  const error = prepareError(object);
-  
-  return winstonLogger.log({
-    private: privateMode,
-    level: level,
-    message: msg, 
-    err: error
-  });
-};
+  /**
+   * Returns the error object, so we always print the full stack of the error
+   * @param  {} err - Error Object
+   */
+  prepareError(error) {
+    if (!error) return {};
 
-/**
- * logs info 
- * @param  {} msg - Any string that needs be printed
- * @param  {} object  - Error object
- * @param  {} privateMode - Whether the log should be written to disk or not 
- */
-const info = function(msg, errorObject, privateMode) {
-  return log(msg, errorObject, 'info', privateMode);
-};
+    const err = JSON.stringify(error) || null;
+    const message = error.message || null;
+    const stack = error.stack || null;
 
-/**
- * Logs errors
- * @param  {} msg - Any string that needs be printed
- * @param  {} object  - Error object
- */
-const error = function(msg, errorObject) {
-  return log(msg, errorObject, 'error');
-};
+    return {
+      err,
+      message,
+      stack,
+    };
+  }
 
-/**
- * Logs warnings
- * @param  {} msg - Any string that needs be printed
- * @param  {} object  - Error object
- */
-const warn = function(msg, errorObject) {
-  return log(msg, errorObject, 'warn');
-};
+  /**
+   * logs info 
+   * @param  {} msg - Any string that needs be printed
+   * @param  {} object  - Error object
+   * @param  {} privateMode - Whether the log should be written to disk or not 
+   */
+  info(msg, errorObject, privateMode) {
+    return this.log(msg, errorObject, 'info', privateMode);
+  }
 
-/**
- * Logs warnings
- * @param  {} msg - Any string that needs be printed
- * @param  {} object  - Error object
- */
-const alert = function(msg, errorObject) {
-  return log(msg, errorObject, 'alert');
-};
+  /**
+   * Logs errors
+   * @param  {} msg - Any string that needs be printed
+   * @param  {} object  - Error object
+   */
+  error(msg, errorObject) {
+    return this.log(msg, errorObject, 'error');
+  }
 
-module.exports = {
-  info, 
-  error,
-  warn,
-  alert
-};
+  /**
+   * Logs warnings
+   * @param  {} msg - Any string that needs be printed
+   * @param  {} object  - Error object
+   */
+  warn(msg, errorObject) {
+    return this.log(msg, errorObject, 'warn');
+  }
+
+  /**
+   * Logs warnings
+   * @param  {} msg - Any string that needs be printed
+   * @param  {} object  - Error object
+   */
+  alert(msg, errorObject) {
+    return this.log(msg, errorObject, 'alert');
+  }
+}
+
+module.exports = new Logger();
 
